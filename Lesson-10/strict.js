433c515b0d8c306216a8e79c16a2d624e9933b13
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-03 12:12:27 +0300

    Add files via upload

---
+++ Lesson-10/strict.js
@@ -0,0 +1,59 @@
+"use script"
+alert("JavaScript lesson practise second")
+//DATA TYPE
+let Name="ism"
+console.log(Name);
+
+let burchak
+console.log(burchak);
+
+let childeren;
+console.log(childeren);
+
+let uchburchak =true
+let aylana =false
+console.log(aylana);
+
+let age="olti"
+console.log(age);
+
+let yosh="25"
+console.log(yosh);
+
+let job ="developer"
+console.log(typeof job);
+
+let Num ="25"
+console.log(typeof Num);
+
+let ism ="Shakhzoda"
+console.log(ism);
+
+let seconName ="Istamova"
+console.log(seconName);
+
+let county ="Uzbekistan"
+console.log(county);
+
+let family ="5-kishi"
+console.log(family);
+
+let year ="19-yosh"
+console.log(year);
+
+let data=`${ism} ${seconName} ${county}da yashayman ${family}dan iborat ${year}daman`;
+console.log(data);
+
+let nme="I am"
+console.log(nme);
+
+let years ="25"
+console.log(years);
+
+let old ="years old"
+console.log(old);
+
+
+
+let date=`${nme} ${years} ${old}`
+console.log(date);
